4c0f094ef43c691ca28ce4cad477aa659f33ca65
Harrison Smith <harrison@hsmith.dev>
2024-08-16 06:34:35 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://vortexlinux.org',
-	base: 'website',
 	integrations: [mdx(), sitemap()],
 });
