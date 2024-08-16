5635b6e466e6d7ca9d19acb37617719666c4d1d0
Harrison Smith <harrison@hsmith.dev>
2024-08-15 22:24:03 -0600

    GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://vortexlinux.org',
+	base: '',
 	integrations: [mdx(), sitemap()],
 });
