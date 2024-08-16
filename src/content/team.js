964cf120643e9f0410015a8e33e2d780a1cc8d91
Harrison Smith <harrison@hsmith.dev>
2024-08-16 05:43:55 -0600

    Added DrProSgtMrJ and configured Titles

--- src/content/team.js
+++ src/content/team.js
@@ -2,25 +2,31 @@
 export const teamMembers = [
     {
       name: 'Arun George Saji',
-      role: 'Founder & Lead Developer',
+      role: 'Founder & Software Engineer',
       imageUrl: 'https://avatars.githubusercontent.com/u/109901706?v=4',
       profileUrl: 'https://github.com/arungeorgesaji',
     },
     {
       name: 'MrInternet',
-      role: 'Title',
+      role: 'Designer',
       imageUrl: 'https://avatars.githubusercontent.com/u/86650985?v=4',
       profileUrl: 'https://github.com/MrInternetGitHub',
     },
     {
       name: 'Mathew4STAR',
-      role: 'Title',
+      role: 'Software Engineer',
       imageUrl: 'https://avatars.githubusercontent.com/u/82754813?v=4',
       profileUrl: 'https://github.com/mathew4STAR',
     },
     {
+      name: 'DrProSgtMrJ',
+      role: 'Software Engineer',
+      imageUrl: 'https://avatars.githubusercontent.com/u/6405049?v=4',
+      profileUrl: 'https://github.com/DrProfSgtMrJ',
+    },
+    {
       name: 'Harrison Smith',
-      role: 'Title',
+      role: 'Software Engineer',
       imageUrl: 'https://avatars.githubusercontent.com/u/42560873?v=4',
       profileUrl: 'https://github.com/hsmith-dev',
     },
