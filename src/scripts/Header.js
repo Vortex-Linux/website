2c3cdce8e15f5487369ff5540d418f0d2d85dfc1
Harrison Smith <harrison@hsmith.dev>
2024-08-16 13:05:44 -0600

    reverted script moving to fix build

--- src/scripts/Header.js
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-document.addEventListener('DOMContentLoaded', function () {
-    const menuButton = document.getElementById('menu-button');
-    const menuLinks = document.querySelector('.menu-links');
-
-    menuButton.addEventListener('click', function () {
-        menuLinks.classList.toggle('open');
-    });
-});
