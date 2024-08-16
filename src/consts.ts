3ec4a1a818531b514ad7d35c7ba71c5015a55b2d
Harrison Smith <harrison@hsmith.dev>
2024-08-15 23:08:42 -0600

    Updates to description

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Vortex Linux';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Explore Vortex Linux, a secure and reliable Linux distribution tailored for both novice and advanced users. Featuring an immutable and atomic operating system with seamless integration of containers and virtual machines, Vortex Linux offers unmatched stability, flexibility, and security. Dive into our vibrant community, discover the latest downloads, and learn more about our mission to empower users with cutting-edge open-source technology.';
 export const SITE_LOGO = '../public/logo.webp';
