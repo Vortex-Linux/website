03a35875d2a7e5214f837097b35ce0adc58a35ea
Harrison Smith <harrison@hsmith.dev>
2024-08-16 08:22:10 -0600

    url fixes for a custom domain on GitHub Pages

--- src/content/social-links.js
+++ src/content/social-links.js
@@ -4,19 +4,19 @@ export const socialLinks = [
       name: 'Read The Docs',
       url: 'https://vortexlinux.readthedocs.io/en/latest/',
       srText: 'Go to Vortex Linux\'s Read The Docs',
-      svgPath: `/website/social-icons/read-the-docs.svg`,
+      svgPath: `/social-icons/read-the-docs.svg`,
     },
     {
       name: 'Discord Server',
       url: 'https://discord.gg/MsF24qUA5y',
       srText: 'Go to Vortex Linux\'s Discord Server',
-      svgPath: `/website/social-icons/discord.svg`,
+      svgPath: `/social-icons/discord.svg`,
     },
     {
       name: 'GitHub Repo',
       url: 'https://github.com/Vortex-Linux/',
       srText: 'Go to Vortex Linux\'s GitHub repo',
-      svgPath: `/website/social-icons/github.svg`,
+      svgPath: `/social-icons/github.svg`,
     }
     // Add more social links as needed
   ];
