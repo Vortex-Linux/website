781cb0785ea8fec1a7ce833eeadc4562903e26a6
Harrison Smith <harrison@hsmith.dev>
2024-08-17 10:30:11 -0600

    navbar optimzation for links

--- src/content/navData.js
+++ src/content/navData.js
@@ -4,16 +4,11 @@ export const navData = {
   logo: "/favicon.svg",
   menu: [
     {
-      title: "Help",
-      dropdown: [
-        { title: "Forum", href: "https://forum.vortexlinux.org" },
-        { title: "ReadTheDocs", href: "https://vortexlinux.readthedocs.io/en/latest/", icon: "/social-icons/read-the-docs.svg" }
-      ]
-    },
-    {
       title: "Info",
       dropdown: [
         { title: "About", href: "/about" },
+        { title: "Forum", href: "https://forum.vortexlinux.org" },
+        { title: "ReadTheDocs", href: "https://vortexlinux.readthedocs.io/en/latest/", icon: "/social-icons/read-the-docs.svg" },
         { title: "Team", href: "/about#team" }
       ]
     },
@@ -24,7 +19,7 @@ export const navData = {
         { title: "Discord", href: "https://github.com/Vortex-Linux/", icon: "/social-icons/discord.svg" },
         { title: "Facebook", href: "https://facebook.com", icon: "/social-icons/facebook.svg" },
         { title: "Instagram", href: "https://instagram.com", icon: "/social-icons/instagram.svg" },
-        { title: "Twitter-X", href: "https://x.com", icon: "/social-icons/x.svg" },
+        { title: "Twitter", href: "https://x.com", icon: "/social-icons/x.svg" },
         { title: "Dev.io", href: "https://dev.io", icon: "/social-icons/dev-io.svg" }
       ]
     }, 
