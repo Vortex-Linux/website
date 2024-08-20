ee7b4e6e9914ebb0cb1508d07d68e2b903b0a482
Harrison Smith <harrison@hsmith.dev>
2024-08-20 17:14:32 -0600

    Social Media Links Updated

--- src/content/data/navData.js
+++ src/content/data/navData.js
@@ -17,11 +17,12 @@ export const navData = {
       title: "Community",
       href: "/blog/get-involved",
       dropdown: [
-        { title: "Discord", href: "https://github.com/Vortex-Linux/"},
+        { title: "Dev.io", href: "https://dev.to/vortexlinux"},
+        { title: "Discord", href: "https://discord.gg/MsF24qUA5y"},
         { title: "Facebook", href: "https://facebook.com"},
         { title: "Instagram", href: "https://instagram.com"},
-        { title: "Twitter", href: "https://x.com"},
-        { title: "Dev.io", href: "https://dev.io"}
+        { title: "Twitter", href: "https://x.com/Vortex_Linux"},
+        { title: "YouTube", href: "https://www.youtube.com/@VortexLinux"}
       ]
     }, 
     {
--- src/content/data/social-links.js
+++ src/content/data/social-links.js
@@ -8,21 +8,27 @@ export const socialLinks = [
     },
     {
       name: 'Instagram',
-      url: 'https://discord.gg/MsF24qUA5y',
+      url: 'https://instagram.com',
       srText: 'Go to Vortex Linux\'s Instagram',
       svgPath: `/social-icons/instagram.svg`,
     },
     {
       name: 'X',
-      url: 'https://github.com/Vortex-Linux/',
+      url: 'https://x.com/Vortex_Linux',
       srText: 'Go to Vortex Linux\'s X',
       svgPath: `/social-icons/x.svg`,
     },
     {
       name: 'Dev',
-      url: 'https://github.com/Vortex-Linux/',
+      url: 'https://dev.to/vortexlinux',
       srText: 'Go to Vortex Linux\'s Dev.io',
       svgPath: `/social-icons/dev-io.svg`,
+    },
+    {
+      name: 'YouTube',
+      url: 'https://www.youtube.com/@VortexLinux',
+      srText: 'Go to Vortex Linux\'s YouTube',
+      svgPath: `/social-icons/youtube.svg`,
     }
     // Add more social links as needed
   ];
